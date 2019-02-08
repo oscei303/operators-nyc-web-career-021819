27285300e9635b0d0d3f0daa9e24b03d2b58cf67
OSCAR RIOS <oscr.rss@gmail.com>
2019-02-08 23:18:00 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+end
+if speed < 40
+  return true 
+end
+else 
+  return false
 end
 
-
-
 def not_safe?(speed)
-	
+  speed > 60 ? true : 
+	speed < 40 ? true : false
 end
 	
 
